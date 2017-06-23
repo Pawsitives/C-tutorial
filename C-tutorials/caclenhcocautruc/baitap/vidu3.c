17ed8914556e6a1184e2469b35862d80fa8ab4bd
thiminhnhut <thiminhnhut@gmail.com>
2017-06-23 08:54:55 +0700

    Vi du 3 - Cau truc re nhanh

---
+++ C-tutorials/caclenhcocautruc/baitap/vidu3.c
@@ -0,0 +1,23 @@
+// Lap trinh C tren Ubuntu su dung Geany
+
+/* Tim nghich dao cua mot so thuc a khac 0
+ */
+
+#include <stdio.h>
+
+int main(){
+	float a;
+	
+	printf("Nhap vao so thuc a: ");
+	scanf("%f",&a);
+	
+	if (a != 0){
+		float nghichdao = (float) 1/a;
+		printf("So nghich dao cua a: %f\n", nghichdao);
+	}
+	else{
+		printf("Khong the tim duoc nghich dao cua a\n");
+	}
+	
+	return 0;
+}
