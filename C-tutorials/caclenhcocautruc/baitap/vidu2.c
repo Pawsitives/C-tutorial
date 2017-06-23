b758d738d86cfd669bb5fa1ed8098afa0996a787
thiminhnhut <thiminhnhut@gmail.com>
2017-06-23 08:50:32 +0700

    Vi du 2 - Cau truc re nhanh

---
+++ C-tutorials/caclenhcocautruc/baitap/vidu2.c
@@ -0,0 +1,20 @@
+// Lap trinh C tren Ubuntu su dung Geany
+
+/* Nhap vao 2 so a, b. Kiem tra a co lon hon b khong
+ */
+
+#include <stdio.h>
+
+int main(){
+	float a, b;
+	
+	printf("Nhap vao 2 so a, b: ");
+	scanf("%f%f",&a, &b);
+	
+	if (a > b){		
+		printf("Gia tri cua a lon hon gia tri cua b\n");
+		printf("Hai so a, b: %0.5f %0.5f", a, b);
+	}
+	
+	return 0;
+}
